d148a7cc87cd6f629e2c7b23f9b3795776071103
Ozodbek <ozodbekhimmatov@gmail.com>
2023-05-28 14:35:52 +0500

    home page UI (complete)

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,11 @@ let focus = ref(false);
 let changeFocus = () => (focus.value = !focus.value);
 </script>
 <template>
-  <div class="flex">
-    <form class="flex gap-2 bg-white rounded-full p-1 items-center" :class="focus ? 'shadow-xl' : ''">
+  <div class="lg:w-[60%] md:w-[80%] sm:w-[100%] mx-auto flex flex-col items-center gap-32 ">
+    <form
+      class="flex gap-2 bg-white rounded-full p-1 items-center w-full"
+      :class="focus ? 'shadow-xl' : ''"
+    >
       <button
         class="bg-[#01756C] text-white text-[16px] rounded-full uppercase py-[20px] px-[36px]"
       >
@@ -13,12 +16,13 @@ let changeFocus = () => (focus.value = !focus.value);
       <img class="ms-5" src="/search.svg" alt="Search" />
       <input
         @focus="changeFocus"
-        class="w-[847px] outline-none rounded-full text-[16px]"
-        type="text"
+        @blur="changeFocus"
+        class="w-[847px] outline-none px-1 rounded-full text-[16px]"
+        type="search"
         placeholder="Search words here..."
       />
     </form>
-    <img src="~/assets/img/search-word.png" alt="">
+    <img class="" src="~/assets/img/search-word.png" alt="" />
   </div>
 </template>
 
